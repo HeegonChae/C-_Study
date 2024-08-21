9b1b321824d8bbcdfceba45750c7690312701502
HeegonChae <113625699+HeegonChae@users.noreply.github.com>
2024-08-22 02:30:19 +0900

    add practice source code

--- 클래스 & 추상화/header/stock17.h
+++ 클래스 & 추상화/header/stock17.h
@@ -1,20 +1,20 @@
-#ifndef STOCK_H     // 기호 상수명(e.g. STRUCT_H , yeah, HEEGON, hee, ... )   
+#ifndef STOCK_H     						// 기호 상수명(e.g. STRUCT_H , yeah, HEEGON, hee, ... )   
 #define STOCK_H
 
 # include <iostream>
 using namespace std;
 
-class Stock  // 사용자 정의된 데이터형 by 클래스 => 'Stock' 
+class Stock  							// 사용자 정의된 데이터형 by 클래스 => 'Stock' 
 {
 
 // 1. private 멤버
 private:
 	string name;
 	int shares;
-	float share_val;					  // ex) 변경 시 => 'update' 매써드 수행
+	float share_val;					  // 변경 시 => 'update' 매써드 수행
 	double total_val;					  // 불가능 => total_val = shares * share_val
 
-	void cal(int Num, float Share_val)   // 'public'내 함수들 통해서 'total_value' 출력만 목적
+	void cal(int Num, float Share_val)   			// 'public'내 함수들 통해서 'total_value' 출력만 목적
 	{
 		total_val = Num*Share_val;
 	}
@@ -26,9 +26,10 @@ public:
 	void sell(int, float);
 	void update(float);
 	void show();
-	// C++ 기본 제공 멤버 함수
+
+// C++ 기본 제공 멤버 함수
 	Stock();							// 디폴트 생성자
 	~Stock();							// 파괴자
 };
 
-#endif //
+#endif // !STOCK_H
