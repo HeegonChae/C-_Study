3d0f3469551fed7f927ae2be079f99d2af0ce6c6
HeegonChae <113625699+HeegonChae@users.noreply.github.com>
2024-08-22 01:54:50 +0900

    Add files via upload

---
+++ 분할 컴파일/header/struct.h
@@ -0,0 +1,15 @@
+
+#ifndef STRUCT_H     // 기호 상수명(e.g. STRUCT_H , yeah, HEEGON, hee, ... )   
+#define STRUCT_H
+
+# include <iostream>
+using namespace std;
+
+struct MyInfo {
+	int age;
+	string name;
+};
+
+void display(MyInfo*);
+
+#endif // !STRUCT_H 
