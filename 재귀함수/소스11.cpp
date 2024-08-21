e8010ab5d915750e954ced4c6b5a9fe5ea8ee476
HeegonChae <113625699+HeegonChae@users.noreply.github.com>
2024-08-22 01:01:02 +0900

    add practice source code

--- 재귀함수/소스11.cpp
+++ 재귀함수/소스11.cpp
@@ -1,24 +1,23 @@
 # include <iostream>
 using namespace std;
 
-// 디폴트 매개변수 선언 방법: 가장 오른쪽에 위치해서 선언할 것
-void CountDown(int n = 3);
+void CountDown(int n = 3);					// 디폴트 매개변수 선언 방법: 가장 오른쪽에 위치해서 선언할 것
 int Mysum(int n = 5);
 
 int main() {
 	CountDown();
 	cout << "\n";
 
-	int(*pt)(int);						// 함수 포인터 선언 소스14.cpp
+	int(*pt)(int);						// 함수 포인터 선언  cf) 소스14.cpp
 	pt = Mysum;
 
-	cout << Mysum << ' ' << pt << "\n"; // 함수명이 함수 포인터
+	cout << Mysum << ' ' << pt << "\n"; 			// 함수명이 함수 포인터
 	cout << Mysum(3) << "\n";
 	cout << pt(3) << "\n";
 	cout << (*pt)(3) << "\n";
 }
 
-void CountDown(int n) {					// 디폴트 파라미터 존재
+void CountDown(int n) {						// 디폴트 파라미터 존재
 	cout << n << "th Code #1" << "\n";
 	if (n > 0)
 		CountDown(n - 1);
