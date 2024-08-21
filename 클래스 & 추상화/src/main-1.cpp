98f924c75a2fecff3b988af8a6be73c7ec0086ec
HeegonChae <113625699+HeegonChae@users.noreply.github.com>
2024-08-22 02:53:15 +0900

    add practice source code

--- 클래스 & 추상화/src/main-1.cpp
+++ 클래스 & 추상화/src/main-1.cpp
@@ -3,7 +3,6 @@
 // 추상화와 클래스_(2) 자동 할당
 int main() {
 
-	//클래스 선언
 	cout << "생성자를 이용하여 객체 생성\n";
 	Stock temp("Chae", 100, 1000);					// or Stock temp = Stock("Chae", 100, 1000);
 
@@ -16,8 +15,8 @@ int main() {
 	
 	cout << "생성자를 이용하여 temp 내용 업데이트\n";
 	
-	// ** 파괴자는 파괴할 당시 name을 출력하는 데, 
-	// ** 파괴자가 호출되었을 때 이미 name의 값은 바뀜
+	// 파괴자는 파괴할 당시 name을 출력하는 데, 
+	// 파괴자가 호출되었을 때 이미 name의 값은 바뀜
 	temp = Stock("Heegon", 200, 800);  
 
 	cout << "재설정된 temp 출력\n";
