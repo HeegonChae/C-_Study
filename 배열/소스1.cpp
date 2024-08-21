46f56b9c08269a76676724b842f5c9ee6d5ced86
HeegonChae <113625699+HeegonChae@users.noreply.github.com>
2024-08-21 23:27:17 +0900

    add practice source code

--- 배열/소스1.cpp
+++ 배열/소스1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 */
 int main()
 {
-	short month[12] = { 1, 2, 3 };
+	short month[12] = { 1, 2, 3 };					// 0000004BFED0FA28
 	short* year;
 
 	year = month;
@@ -32,19 +32,19 @@ int main()
 	B = &A;
 
 	cout << a << "\n";
-	cout << sizeof(a) << "\n";				// 배열 메모리 상 크기: char 자료형 x 6 = 6
+	cout << sizeof(a) << "\n";					// 배열 메모리 상 크기: char 자료형 x 7 = 7
 	cout << sizeof(a) / sizeof(char) << "\n";
 	cout << b << "\n";
-	cout << sizeof(b) << "\n";              // 포인터 크기 = 8
-	cout << sizeof(b) / sizeof(char) << "\n";
+	cout << sizeof(b) << "\n";              			// 포인터 크기 = 8
+	cout << sizeof(b) / sizeof(char) << "\n";			
 	cout << "\n";
 
 	cout << A << "\n";
-	cout << sizeof(A) << "\n";				// string 객체 메모리 상 크기: 40
-	cout << sizeof(A[6]) << "\n";			// string 객체 요소 메모리 크기 = char형 객체 메모리 크기
-	cout << sizeof(A) / sizeof(string) << "\n";
+	cout << sizeof(A) << "\n";					// string 객체 메모리 상 크기: 40
+	cout << sizeof(A[6]) << "\n";					// string 객체 요소 메모리 크기 = char형 객체 메모리 크기 = 1 
+	cout << sizeof(A) / sizeof(string) << "\n";			// 1
 	cout << *B << "\n";
-	cout << sizeof(B) << "\n";              // 포인터 크기 = 8
+	cout << sizeof(B) << "\n";              			// 포인터 크기 = 8
 	cout << sizeof(B) / sizeof(string) << "\n";
 	cout << "\n";
 }
